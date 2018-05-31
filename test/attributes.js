c9f1d9c4ffed8de5004d48c9899008d7de2bb9c2
Chris Manson <chris@stonecircle.io>
2018-05-31 15:40:59 +0100

    adding tests to check ability to define contentTypes

--- test/attributes.js
+++ test/attributes.js
@@ -1,5 +1,117 @@
-describe('JSONAPI attributes', () => {
+const { createBuilder, createTempDir } = require('broccoli-test-helper');
+const { expect } = require('chai');
+
+const StaticSiteJson = require('../index');
+
+let output;
+let input;
+
+async function buildSingleFile(fileContents, options) {
+  input.write({
+    'index.md': fileContents,
+  });
+
+  const subject = new StaticSiteJson(input.path(), options);
+  output = createBuilder(subject);
+
+  await output.build();
+
+  const folderOutput = output.read();
+  const indexJSON = JSON.parse(folderOutput.content['index.json']);
+  return indexJSON.data;
+}
+
+describe.only('JSONAPI attributes', () => {
+  beforeEach(async () => {
+    input = await createTempDir();
+  });
+
+  afterEach(async () => {
+    try {
+      await input.dispose();
+    } finally {
+      // do nothing
+    }
+
+    if (output) {
+      await output.dispose();
+    }
+  });
+
   it('should include title if the frontmatter is included but no options passed');
   it('should not inlude any extra frontmatter that is not defined in attributes');
   it('should allow you to specify attributes and for them to be included in the output');
+
+  it('should only include html and content with basic config', async () => {
+    const result = await buildSingleFile('# Hello world');
+
+    expect(result.attributes).have.keys(['html', 'content']);
+  });
+
+  it('should not include html if it is not in the content types', async () => {
+    const result = await buildSingleFile('# Hello world', {
+      contentTypes: ['content'],
+    });
+
+    expect(result.attributes).have.keys(['content']);
+  });
+
+  it('should not include markdown if it is not in the content types', async () => {
+    const result = await buildSingleFile('# Hello world', {
+      contentTypes: ['html'],
+    });
+
+    expect(result.attributes).have.keys(['html']);
+  });
+
+  it('should include description if it is defined in config', async () => {
+    const result = await buildSingleFile('# Hello world', {
+      contentTypes: ['html', 'content', 'description'],
+    });
+
+    expect(result.attributes).have.keys(['html', 'content', 'description']);
+  });
+
+  it('should limit description to 200 characters', async () => {
+    const result = await buildSingleFile(`# Hello world
+
+This is where I write my really long essay to the world. I will start off bing **super** important and then _slow down_ to a stop.
+
+## Second point
+
+I really like programming. I could do this all day long without ever stooopping, no matter how long the word limit is`, {
+      contentTypes: ['html', 'content', 'description'],
+    });
+
+    expect(result.attributes.description).to.have.lengthOf.at.most(260);
+  });
+
+  it('should limit description to 200 characters', async () => {
+    const result = await buildSingleFile(`# Hello world
+
+This is where I write my really long essay to the world. I will start off bing **super** important and then _slow down_ to a stop.
+
+## Second point
+
+I really like programming. I could do this all day long without ever stooopping, no matter how long the word limit is`, {
+      contentTypes: ['html', 'content', 'description'],
+    });
+
+    expect(result.attributes.description, 'does not end with ...').to.match(/\.\.\.$/);
+  });
+
+  it('should throw an error if there is an unknown content type', async () => {
+    let error;
+
+    try {
+      await buildSingleFile('# Hello world', {
+        contentTypes: ['html', 'content', 'faceyFace'],
+      });
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error, 'Build did not error').to.be.ok;
+    expect(error.message).to.equal('Unknown content type: faceyFace');
+  });
 });
