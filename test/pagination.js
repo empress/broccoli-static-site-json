c26e5e9ccbaecb6bbc39ca0fa7eda96e0d49dc8c
Chris Manson <chris@manson.ie>
2019-04-11 14:18:30 +0100

    adding pagination links

--- test/pagination.js
+++ test/pagination.js
@@ -135,4 +135,178 @@ title: duplicate
     expect(JSON.parse(folderOutput.content['all-0.json']).data).to.have.length(10);
     expect(JSON.parse(folderOutput.content['all-1.json']).data).to.have.length(2);
   });
+
+  it('should setup pagination links correctly', async () => {
+    const mdFiles = new Funnel(input.path(), { destDir: 'face' });
+
+    const subject = new StaticSiteJson(mdFiles, {
+      type: 'page',
+      collate: true,
+      paginate: true,
+      pageSize: 3,
+    });
+
+    output = createBuilder(subject);
+
+    input.write({
+      'index.md': `---
+title: a lovely title
+---
+# Hello world`,
+      'project.md': `---
+title: a less lovely title
+---
+# Goodbye world`,
+      'double-word.md': `---
+title: more words
+---
+# When one word is not enough`,
+      '1.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '2.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '3.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '4.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '5.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '6.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '7.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '8.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '9.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+    });
+
+    await output.build();
+
+    const folderOutput = output.read();
+    const pageOne = JSON.parse(folderOutput.content['all-0.json']);
+    const pageFour = JSON.parse(folderOutput.content['all-3.json']);
+
+    expect(pageOne.data).to.have.length(3);
+    expect(pageOne.links).to.deep.equal({
+      first: '/content/all-0.json',
+      last: '/content/all-3.json',
+      prev: null,
+      next: '/content/all-1.json',
+    });
+
+    expect(pageFour.data).to.have.length(3);
+    expect(pageFour.links).to.deep.equal({
+      first: '/content/all-0.json',
+      last: '/content/all-3.json',
+      prev: '/content/all-2.json',
+      next: null,
+    });
+  });
+
+  it('should prefix pagination links with the right contentFolder', async () => {
+    const mdFiles = new Funnel(input.path(), { destDir: 'face' });
+
+    const subject = new StaticSiteJson(mdFiles, {
+      type: 'page',
+      collate: true,
+      paginate: true,
+      pageSize: 3,
+      contentFolder: 'facey-face',
+    });
+
+    output = createBuilder(subject);
+
+    input.write({
+      'index.md': `---
+title: a lovely title
+---
+# Hello world`,
+      'project.md': `---
+title: a less lovely title
+---
+# Goodbye world`,
+      'double-word.md': `---
+title: more words
+---
+# When one word is not enough`,
+      '1.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '2.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '3.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '4.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '5.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '6.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '7.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '8.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+      '9.md': `---
+title: duplicate
+---
+# don't write duplicate content`,
+    });
+
+    await output.build();
+
+    const folderOutput = output.read();
+
+    const pageOne = JSON.parse(folderOutput['facey-face']['all-0.json']);
+    const pageFour = JSON.parse(folderOutput['facey-face']['all-3.json']);
+
+    expect(pageOne.data).to.have.length(3);
+    expect(pageOne.links).to.deep.equal({
+      first: '/facey-face/all-0.json',
+      last: '/facey-face/all-3.json',
+      prev: null,
+      next: '/facey-face/all-1.json',
+    });
+
+    expect(pageFour.data).to.have.length(3);
+    expect(pageFour.links).to.deep.equal({
+      first: '/facey-face/all-0.json',
+      last: '/facey-face/all-3.json',
+      prev: '/facey-face/all-2.json',
+      next: null,
+    });
+  });
 });
