0dcbff28dba55ac1966750e4289839a24de9e4ed
Chris Manson <chris@manson.ie>
2019-07-23 09:46:56 +0100

    remove support for overriding id

--- test/collections.js
+++ test/collections.js
@@ -76,39 +76,6 @@ title: more words
     expect(files['all.json'].find(obj => obj.id === 'double-word')).to.be.ok;
   });
 
-  it('should name the files the same as the id if the id is specified', async () => {
-    const files = await buildFiles({
-      'index.md': `---
-title: a lovely title
-id: 1
----
-# Hello world`,
-      'project.md': `---
-title: a less lovely title
-id: 2
----
-# Goodbye world`,
-      'double-word.md': `---
-title: more words
-id: 3
----
-# When one word is not enough`,
-    }, {
-      collate: true,
-    });
-
-    expect(files).to.have.property('1.json');
-
-    expect(files['1.json']).to.have.property('id', '1');
-    expect(files['2.json']).to.have.property('id', '2');
-    expect(files['3.json']).to.have.property('id', '3');
-
-    // each of the above files should exist in the all.json
-    expect(files['all.json'].find(obj => obj.id === '1')).to.be.ok;
-    expect(files['all.json'].find(obj => obj.id === '2')).to.be.ok;
-    expect(files['all.json'].find(obj => obj.id === '3')).to.be.ok;
-  });
-
   it('should allow you to define a collection and for the specified content folder to be exported as an single JSONAPI array response', async () => {
     const subject = new StaticSiteJson(input.path(), {
       attributes: ['title'],
