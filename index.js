f1c399b4a6c0b648abe2331eab36dd2eac79d97d
billybonks <sebastienstettler@gmail.com>
2019-10-11 18:22:32 +0800

    add config options

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ module.exports = function StaticSiteJson(folder, options = {}) {
   const cleanMarkdownFunnel = new BroccoliFunnel(folder, {
     include: ['**/*.md', '**/*.markdown'],
   });
-  const pagesTree = new TableOfContents([folder], options);
-  const jsonApiTree = new MarkdownToJsonApi(cleanMarkdownFunnel, options);
+
+  let jsonApiTree = new MarkdownToJsonApi(cleanMarkdownFunnel, options);
+  if (options.postProcessContentTree) {
+    jsonApiTree = options.postProcessContentTree(jsonApiTree);
+  }
+  const pagesTree = options.buildToc
+    ? options.buildToc(cleanMarkdownFunnel, jsonApiTree, options)
+    : new TableOfContents([folder], options);
   // TODO finish implementing collation tree
   // const collationTree = new CollateJsonApiBlobs(jsonTree, options);
   const compiledTrees = new BroccoliMergeTrees([jsonApiTree, pagesTree]);
