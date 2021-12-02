815dde46af07e51b91b2f16028c5c08423d0a6a7
Chris Manson <chris@manson.ie>
2021-12-02 10:45:28 +0000

    simplify table-of-contents test

--- test/plugins/table-of-contents.js
+++ test/plugins/table-of-contents.js
@@ -5,59 +5,62 @@ const { expect } = require('chai');
 const TableOfContents = require('../../lib/table-of-contents');
 
 describe('table-of-contents', function () {
-  it('should build', async function () {
-    const input = await createTempDir();
-    try {
-      const subject = new TableOfContents(input.path());
-      const output = createBuilder(subject);
+  let input;
+  let output;
 
-      try {
-        // INITIAL
-        input.write({
-          'pages.yml': `- title: "Getting Started"
+  beforeEach(async function () {
+    input = await createTempDir();
+  });
+
+  afterEach(async function () {
+    await input.dispose();
+    await output.dispose();
+  });
+
+  it('should build pages.yml', async function () {
+    const subject = new TableOfContents(input.path());
+    output = createBuilder(subject);
+
+    // INITIAL
+    input.write({
+      'pages.yml': `- title: "Getting Started"
   url: 'getting-started'
   pages:
     - title: "How To Use The Guides"
       url: "intro"`,
-        });
+    });
 
-        await output.build();
+    await output.build();
 
-        expect(output.read()).to.deep.equal({
-          'pages.json': '{"data":[{"type":"pages","id":"getting-started","attributes":{"title":"Getting Started","pages":[{"title":"How To Use The Guides","url":"getting-started/intro"}]}}]}',
-        });
+    expect(output.read()).to.deep.equal({
+      'pages.json': '{"data":[{"type":"pages","id":"getting-started","attributes":{"title":"Getting Started","pages":[{"title":"How To Use The Guides","url":"getting-started/intro"}]}}]}',
+    });
 
-        expect(output.changes()).to.deep.equal({
-          'pages.json': 'create',
-        });
+    expect(output.changes()).to.deep.equal({
+      'pages.json': 'create',
+    });
 
-        // UPDATE
-        input.write({
-          'pages.yml': `- title: "Tutorial"
+    // UPDATE
+    input.write({
+      'pages.yml': `- title: "Tutorial"
   url: 'tutorial'
   pages:
     - title: "Creating Your App"
       url: "ember-cli"`, // change
-        });
-        await output.build();
+    });
+    await output.build();
 
-        expect(output.read()).to.deep.equal({
-          'pages.json': '{"data":[{"type":"pages","id":"tutorial","attributes":{"title":"Tutorial","pages":[{"title":"Creating Your App","url":"tutorial/ember-cli"}]}}]}',
-        });
+    expect(output.read()).to.deep.equal({
+      'pages.json': '{"data":[{"type":"pages","id":"tutorial","attributes":{"title":"Tutorial","pages":[{"title":"Creating Your App","url":"tutorial/ember-cli"}]}}]}',
+    });
 
-        expect(output.changes()).to.deep.equal({
-          'pages.json': 'change',
-        });
+    expect(output.changes()).to.deep.equal({
+      'pages.json': 'change',
+    });
 
-        // NOOP
-        await output.build();
+    // NOOP
+    await output.build();
 
-        expect(output.changes()).to.deep.equal({});
-      } finally {
-        await output.dispose();
-      }
-    } finally {
-      await input.dispose();
-    }
+    expect(output.changes()).to.deep.equal({});
   });
 });
